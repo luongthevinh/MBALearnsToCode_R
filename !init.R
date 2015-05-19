e57077e1ff36923bb183a3088fc84fc2a46a5a0c
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-05-19 12:42:44 -0500

    init commit

---
+++ !init.R
@@ -0,0 +1,70 @@
+setwd("C:/Cloud/DropBox/MBALearnsToCode/R")
+
+library(ada) ###
+      # ada: an R package for stochastic boosting
+library(caret) ###
+      # Classification and Regression Training
+library(data.table) ###
+      # Extension of data.frame
+library(devtools) ###
+      # Tools to make developing R code easier
+library(e1071) ###
+      # Misc Functions of the Department of Statistics (e1071), TU Wien
+library(elasticnet) ###
+      # Elastic-Net for Sparse Estimation and Sparse PCA
+library(forecast) ###
+      # Forecasting functions for time series and linear models
+library(gam) ###
+      # Generalized Additive Models
+library(GAMBoost) ###
+      # Generalized linear and additive models by likelihood based boosting
+library(gbm) ###
+      # Generalized Boosted Regression Models
+library(ggplot2) ###
+      # An implementation of the Grammar of Graphics
+library(googleVis) ###
+      # Interface between R and the Google Chart Tools
+library(knitr) ###
+      # A general-purpose package for dynamic report generation in R
+library(lattice)
+      # Lattice Graphics
+library(latticeExtra)
+      # Extra Graphical Utilities Based on Lattice
+library(markdown) ###
+      # Markdown rendering for R
+library(mboost)
+      # Model-Based Boosting
+library(neuralnet)
+      # Training of neural networks
+library(nnet)
+      # Feed-forward Neural Networks and Multinomial Log-Linear Models
+library(plyr)
+      # Tools for splitting, applying and combining data
+library(randomForest)
+      # Breiman and Cutler's random forests for classification and regression
+library(rcharts)
+      # [...]
+library(reshape2)
+      # Flexibly reshape data: a reboot of the reshape package
+library(rpart) ###
+      # Recursive Partitioning and Regression Trees
+library(rpart.plot)
+      # Plot rpart models. An enhanced version of plot.rpart
+library(RSNNS)
+      # Neural Networks in R using the Stuttgart Neural Network Simulator (SNNS)
+library(shiny)
+      # Web Application Framework for R
+library(shinyapps)
+      #
+library(slidify)
+      #
+library(slidifyLibraries)
+      #
+library(sqldf)
+      # Perform SQL Selects on R Data Frames
+library(tree)
+      # Classification and regression trees
+library(xlsx)
+      # Read, write, format Excel 2007 and Excel 97/2000/XP/2003 files
+library(XML)
+      # Tools for parsing and generating XML within R and S-Plus
---
+++ !instPkgs.R
@@ -0,0 +1,106 @@
+install.packages(c(
+      'abind', 'acepack', 'actuar', 'ada', 'ADGofTest', 'AER', 'AGD', 'AICcmodavg',
+            'akima', 'Amelia', 'animation','ape', 'arm', 'arules', 'ascii', 'assertthat',
+      'BayesX', 'BB', 'bbmle', 'bdsmatrix', 'betareg', 'biclust', 'biglm', 'bit', 'bit64',
+            'bitops', 'BMA', 'BradleyTerry2', 'brew', 'BRugs',
+      'ca', 'caTools', 'cacheSweave', 'Cairo', 'candisc', 'car', 'caret', 'catdata',
+            'cba', 'cem', 'chron', 'clue', 'coda', 'combinat', 'CompQuadForm',
+            'coin', 'colorspace', 'copula', 'corpcor', 'corrgram', 'corrplot', 'coxme',
+            'cubature', 'Cubist',
+      'DAAG', 'data.table', 'date', 'DBI', 'deldir', 'denstrip', 'DEoptimR', 'DescTools', 'deSolve',
+            'devtools', 'dfoptim', 'dichromat', 'DierckxSpline', 'digest', 'diptest', 'distrom',
+            'DistributionUtils', 'dmr', 'doBy', 'doMC', 'doParallel', 'doSNOW', 'dynlm', 'dplyr',
+      'e1071', 'earth', 'Ecdat', 'Ecfun', 'effects', 'elasticnet', 'ellipse',
+            'emdbook', 'entropy', 'evaluate', 'expm', 'expsmooth',
+      'Fahrmeir', 'fastICA', 'fastmatch', 'fBasics', 'fda', 'fdrtool', 'fft',
+            'fields', 'filehash', 'fit.models', 'flexclust', 'flexmix', 'fma',
+            'FMStable', 'foreach', 'forecast','formatR', 'Formula', 'fpp', 'fracdiff', 'FRB', 'fTrading',
+      'gam', 'GAMBoost', 'gamlr', 'gamlss', 'gamlss.data', 'gamlss.dist', 'gamm4',
+            'gclus', 'gdata', 'gbm', 'gee', 'geepack', 'GeneralizedHyperbolic',
+            'geoR', 'geoRglm', 'GGally', 'ggplot2', 'glasso', 'glmmML', 'glmnet',
+            'gmodels', 'gmp', 'gnm' , 'gof', 'googleVis', 'GPArotation', 'gplots', 'grid', 'gridBase', 'gridExtra',
+            'gsl', 'gss',  'gtable', 'gtools',
+      'hash', 'heplots', 'hexbin', 'highr', 'Hmisc', 'HSAUR', 'htmltools', 'httpuv', 'httr', 'huge',
+      'igraph', 'igraphdata', 'ineq', 'inline', 'ipred', 'isa2', 'ISwR', 'iterators', 
+            'itertools', 'its',
+      'jpeg', 'jsonlite', 
+      'kernlab', 'kinship2', 'kknn', 'klaR', 'knitr', 'ks',
+      'labeling', 'Lahman', 'lars', 'lattice', 'latticeExtra', 'lava', 'lava.tobit', 'lavaan',
+            'lavaan.survey', 'lazyeval', 'leaps', 'LearnBayes', 'lhs', 'LICORS', 'lisrelToR', 'lme4', 'lmtest',
+            'locfit', 'logcondens', 'logspline', 'lokern', 'longmemo',
+            'lpSolve', 'lqa', 'lubridate',
+      'magic', 'magrittr', 'mapproj', 'maps', 'maptools', 'maptpx', 'markdown', 'Matching', 'MatchIt', 'Matrix',
+            'matrixcalc', 'MatrixModels', 'maxLik', 'maxlike', 'MBESS', 'mboost',
+            'mclust', 'MCMCpack', 'mda', 'memoise', 'mets', 'microbenchmark',
+            'mi', 'mice', 'misc3d', 'miscTools', 'mitools', 'mix', 'mlbench',
+            'mlogit', 'mnormt', 'modeltools', 'mondate', 'MplusAutomation', 'MPV',
+            'mratios', 'multcomp', 'multicool', 'MuMIn', 'munsell', 'mvtnorm',
+            'mvnormtest',
+      'ncdf', 'network', 'neuralnet', 'nloptr', 'nnet', 'nnls', 'nor1mix', 'np',
+            'numDeriv', 'nws', 'nycflights13',
+      'optextras', 'optmatch', 'optimx', 'ordinal',
+      'pamr', 'pan', 'partitions', 'party', 'pbivnorm', 'PBSmapping', 'pcaPP', 'penalized',
+            'pbkrtest',  'PerformanceAnalytics', 'pixmap', 'plm', 'plotmo', 'plotrix', 'png', 'poLCA',  'polyclip',
+            'polspline', 'polycor', 'polynom', 'PolynomF', 'prodlim', 'pls',
+            'plyr', 'pROC', 'profileModel', 'proto', 'proxy', 'pryr', 'pscl', 'pspline', 'psych',
+      'qgraph', 'gsubfn', 'quadprog', 'quantreg', 'qvcalc',
+      'R.matlab', 'R.methodsS3', 'R.oo', 'R.utils', 'R2jags', 'R2HTML',
+            'R2OpenBUGS', 'R2WinBUGS', 'R6', 'RandomFields', 'randomForest', 'RANN',
+            'raster', 'rasterVis', 'rbounds', 'Rcgmin', 'RColorBrewer', 'Rcpp', 'RcppArmadillo',
+            'Rcsdp', 'RCurl', 'relations', 'reshape', 'reshape2', 'rgenoud', 'rgeos', 'rgdal',
+            'rgl', 'Rglpk', 'RH2', 'RItools', 'rjags', 'rJava', 'RJDBC', 'rjson', 'RJSONIO', 
+            'rlecuyer', 'Rmalschains', 'rmarkdown', 'rmeta', 'Rmpfr', 'Rmpi', 'rms',
+            'rockchalk', 'ROCR', 'RODBC', 'robust', 'robustbase', 'roxygen2',
+            'rpanel', 'rpart', 'rpart.plot', 'RPostgreSQL', 'rPython', 'rrcov', 'RSNNS',
+            'Rsolnp', 'rsprng', 'RSQLite', 'RSQLite.extfuns', 'rstudioapi',
+            'Rsymphony', 'rtiff', 'rugarch', 'RUnit', 'Rvmmin',
+      'sampleSelection', 'sandwich', 'scales', 'scatterplot3d', 'scrypt', 'sem',
+            'semPlot', 'semTools', 'seriation', 'setRNG', 'sets', 'sfsmisc',
+            'shapefiles', 'shiny', 'simsem', 'SimComp', 'SkewHyperbolic',
+            'slam', 'Sleuth2', 'sm', 'sna', 'snow', 'snowfall', 'som', 'sp', 'spam',
+            'SparseM', 'spatstat', 'spd', 'spdep', 'splancs', 'splines', 'spls', 'sqldf',
+            'stashR', 'statmod', 'statnet.common', 'stabledist', 'stringr',
+          'strucchange', 'subselect',
+            'superpc', 'SuppDists', 'survey', 'survival', 'svd', 'svGUI', 'svUnit', 'systemfit',
+      'tables', 'tcltk2', 'TeachingDemos',  'tensor', 'testit', 'testthat', 'textir', 'TH.data',
+            'tikzDevice', 'timeDate', 'timereg', 'timeSeries', 'tis', 'tkrplot',
+            'tree', 'tripack', 'truncnorm', 'truncreg', 'TSA', 'tseries', 
+            'TSP', 'tweedie',
+      'ucminf', 'unmarked', 'urca',
+      'VarianceGamma', 'vcd', 'vcdExtra', 'VGAM', 'VGAMdata', 'vioplot',
+      'WhatIf', 'whisker', 'wordcloud', 
+      'xlsx', 'xlsxjars', 'XML', 'xtable', 'xts',
+      'yaml',
+      'Zelig', 'zoo'),
+      dependencies = TRUE)
+
+#'stringr', 'mime', 'stabs', 'texreg', 'relimp', 'brglm', 'minqa', 'sendplot', 'd3Network',
+#'ggm', 'dichromat', , 'munsell', 'labeling', 
+
+#'intervals', 'rex', 'crayon', 'stringdist', 'spacetime', 'FNN', 'prefmod', 'alr4', 'lintr',
+#''lazyeval', 'RMySQL', 'Lahman', 'nycflights13', 'TeachingDemos',
+#''stringi', 'BMA', 'DescTools', 'gridBase', 'pryr', 'actuar', 'base64enc', 'curl',
+#''formatR', 'highr', 'minqa', 'PKPDmodels', 'MEMSS', 'mlmRev', 'stabs', 'texreg', 'relimp',
+#' 'sendplot', 'd3Network', 'ggm', 'rbenchmark', 'highlight', 'pkgKitten', 'gridExtra',
+#''GGally', 'gstat', 'goftest', 'cmprsk',   'fts', 
+#''TTR', 'forward', 'oz', 'RDCOMClient', 'intergraph', 'scagnostics', 'tnet', 'RGraphics', 'zipcode', 'sn', 'quantmod', 'R2wd'
+#'
+
+# There is a binary version available (and will be installed)
+# but the source version is later:
+# Error in install.packages : duplicate row.names: gsubfn
+
+# Install RMySQL Package
+      # Make sure R version used matches operating system's 32-bit/64-bit
+      # Create 'Reviron.site' file in '[...]\R\R-3.1.0\etc\'
+      #     w/ content MYSQL_HOME = '[...]/MySQL/MySQL Server 5.6'
+      # Copy '[...]\MySQL\MySQL Server 5.6\lib\libmysql.dll' to:
+      #     '[...]\R\R-3.1.0\bin\' & '[...]\MySQL\MySQL Server 5.6\bin\'
+      # Copy '[...]\MySQL\MySQL Server 5.6\lib\libmysql.lib' to
+      #     '[...]\MySQL\MySQL Server 5.6\lib\opt\'
+#install.packages('RMySQL', type = 'source')
+
+library(devtools)
+install_github("amplab-extras/SparkR-pkg", subdir="pkg")
+install_github(c('rcharts', 'slidify', 'slidifyLibraries'), 'ramnathv')
+install_github('rstudio/shinyapps')
