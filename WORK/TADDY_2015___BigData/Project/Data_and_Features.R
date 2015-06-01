9905e1420b4cc2fb6a4fdff01fdfd5944d5f8104
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2015-06-01 02:16:08 -0500

    update Big Data work

--- WORK/TADDY_2015___BigData/Project/Data_and_Features.R
+++ WORK/TADDY_2015___BigData/Project/Data_and_Features.R
@@ -1,23 +1,39 @@
 library(data.table)
 library(plyr)
-options(expressions = 500000)
 
 
-list_drivers <- function(data_folder_path)
-{
+list_drivers <- function(data_folder_path) {
   list.dirs(path=data_folder_path, full.names=FALSE, recursive=FALSE)
 }
 
 
-read_driver_trip <- function(data_folder_path, driver, trip)
-{
-  library(data.table)
+read_driver_trip <- function(data_folder_path, driver, trip) {
   fread(file.path(data_folder_path, driver, paste(trip, '.csv', sep='')))
 }
 
 
-calc_velocity_vector <- function(trip_data_table)
-{
+euclidean_norm <- function(x1, y1, x2 = 0, y2 = 0) {
+  ((x2 - x1) ^ 2 + (y2 - y1) ^ 2) ^ 0.5
+}
+
+
+scalar_product <- function(x1, y1, x2 = 0, y2 = 0) {
+  x1 * x2 + y1 * y2
+}
+
+
+angular_difference <- function(from_angle, to_angle) {
+  difference <- to_angle - from_angle
+  atan2(sin(difference), cos(difference))
+}
+
+
+circular_arc_length <- function(euclidean_distance_between_ends, angle_between_tangents) {
+  r = (euclidean_distance_between_ends / 2) / cos(angle_between_tangents / 2)
+  (pi - abs(angle_between_tangents)) * r
+}
+
+calc_velocity_vector <- function(trip_data_table) {
   trip_data_table[, `:=`(dx = c(NA, diff(x)),
                          dy = c(NA, diff(y)))]
   trip_data_table$dx[1] = trip_data_table$dx[2]
@@ -26,8 +42,7 @@ calc_velocity_vector <- function(trip_data_table)
 }
   
 
-calc_acceleration_vector <- function(trip_data_table)
-{
+calc_acceleration_vector <- function(trip_data_table) {
   trip_data_table[, `:=`(ddx = c(NA, diff(dx)),
                          ddy = c(NA, diff(dy)))]
   trip_data_table$ddx[1] = trip_data_table$ddx[2]
@@ -36,29 +51,25 @@ calc_acceleration_vector <- function(trip_data_table)
 }
 
 
-calc_velocity <- function(trip_data_table)
-{
+calc_velocity <- function(trip_data_table) {
   trip_data_table[, velocity := (dx ^ 2 + dy ^ 2) ^ 0.5]
   trip_data_table
 }
  
 
-calc_acceleration <- function(trip_data_table)
-{
-  trip_data_table[, acceleration := (ddx ^ 2 + ddy ^ 2) ^ 0.5]
+calc_acceleration <- function(trip_data_table) {
+  trip_data_table[, acceleration := scalar_product(ddx, ddy, dx, dy) / euclidean_norm(dx, dy)]
   trip_data_table
 }
 
 
-calc_angle <- function(trip_data_table)
-{
+calc_angle <- function(trip_data_table) {
   trip_data_table[, angle := atan2(dy, dx)]
   trip_data_table
 }
 
 
-calc_angular_velocity <- function(trip_data_table)
-{
+calc_angular_velocity <- function(trip_data_table) {
   angular_differences = c(NA, diff(trip_data_table$angle))
   angular_differences[1] = angular_differences[2]
   trip_data_table[, angular_velocity := atan2(sin(angular_differences), cos(angular_differences))]
@@ -66,38 +77,34 @@ calc_angular_velocity <- function(trip_data_table)
 }
 
 
-calc_angular_acceleration <- function(trip_data_table)
-{
+calc_angular_acceleration <- function(trip_data_table) {
   trip_data_table[, angular_acceleration := c(NA, diff(angular_velocity))]
   trip_data_table$angular_acceleration[1] = trip_data_table$angular_acceleration[2]
   trip_data_table  
 }
 
 
-calc_velocity_validity <- function(trip_data_table, max_velocity_m_per_s=50)
-{
+calc_velocity_validity <- function(trip_data_table, max_velocity_m_per_s=50) {
   trip_data_table[, velocity_check := velocity < max_velocity_m_per_s]
   trip_data_table
 }
 
 
 calc_angular_velocity_validity <- function(trip_data_table,
-                                           max_absolute_angular_velocity = 5 / 6 * pi)
-{
-  trip_data_table[, angular_velocity_check := abs(angular_velocity) < max_absolute_angular_velocity]
+                                           max_absolute_angular_velocity = 5 / 6 * pi) {
+  trip_data_table[, angular_velocity_check := 
+                    abs(angular_velocity) < max_absolute_angular_velocity]
   trip_data_table
 }
 
 
-calc_overall_data_validity <- function(trip_data_table)
-{
+calc_overall_data_validity <- function(trip_data_table) {
   trip_data_table[, overall_check := velocity_check & angular_velocity_check]
   trip_data_table
 }
 
 calc_trip_data <- function(trip_data_table, max_velocity_m_per_s = 50,
-                           max_absolute_angular_velocity = 5 / 6 * pi)
-{
+                           max_absolute_angular_velocity = 5 / 6 * pi) {
   calc_overall_data_validity(
     calc_angular_velocity_validity(
     calc_velocity_validity(
@@ -113,14 +120,12 @@ calc_trip_data <- function(trip_data_table, max_velocity_m_per_s = 50,
 }
   
 
-check_trip_data_quality <- function(trip_data_table)
-{
+check_trip_data_quality <- function(trip_data_table) {
   all(trip_data_table$overall_check)
 }
 
 
-bad_trip_data_indices <- function(trip_data_table)
-{
+bad_trip_data_indices <- function(trip_data_table) {
   which(!trip_data_table$overall_check)
 }
 
@@ -166,16 +171,7 @@ clean_velocity_data <- function(trip_data_table, num_rows_before_after = 3) {
 }
 
 
-angular_difference <- function(from_angle, to_angle) {
-  difference <- to_angle - from_angle
-  atan2(sin(difference), cos(difference))
-}
 
-
-circular_arc_length <- function(euclidean_distance_between_ends, angle_between_tangents) {
-  r = (euclidean_distance_between_ends / 2) / cos(angle_between_tangents / 2)
-  (pi - abs(angle_between_tangents)) * r
-}
 
 
 interpolate_next_xy_velocity_angle <- function(x1, y1, v1, a1, x2, y2, v2, a2) {
@@ -184,7 +180,7 @@ interpolate_next_xy_velocity_angle <- function(x1, y1, v1, a1, x2, y2, v2, a2) {
   straight_line_angular_diff <- angular_difference(a1, straight_line_angle)
   if ((straight_line_angular_diff > min(0, angular_diff)) &
         (straight_line_angular_diff < max(0, angular_diff))) {
-    arc_length <- circular_arc_length(((x2 - x1) ^ 2 + (y2 - y1) ^ 2) ^ 0.5, angular_diff)
+    arc_length <- circular_arc_length(euclidean_norm(x1, y1, x2, y2), angular_diff)
     mean_velocity <- (v1 + v2) / 2
     estimated_num_seconds <- floor(arc_length / mean_velocity)
     if (estimated_num_seconds > 0) {
@@ -199,7 +195,7 @@ interpolate_next_xy_velocity_angle <- function(x1, y1, v1, a1, x2, y2, v2, a2) {
       NULL
     }
   } else {
-    euclidean_distance <- ((x2 - x1) ^ 2 + (y2 - y1) ^ 2) ^ 0.5
+    euclidean_distance <- euclidean_norm(x1, x2, y1, y2)
     mean_velocity <- (v1 + v2) / 2
     estimated_num_seconds <- floor(euclidean_distance / mean_velocity)
     if (estimated_num_seconds > 0) {
